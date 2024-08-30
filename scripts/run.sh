4e3805ba34e875ba69e7e6b1d22741d846c9610f
zhangzw16 <thuzhangzw@outlook.com>
2024-08-30 02:12:23 +0000

    add grid run amlt

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,11 @@
+DEVICE=0
+export CUDA_VISIBLE_DEVICES=${DEVICE}
+
+# take input arguments as DATASET
+DATASET=${1}
+
 python -m cli.train \
   -cp conf/pretrain \
-  run_name=ltsf_pretrain \
+  run_name=${DATASET} \
   model=moirai_small \
-  data=ltsf
+  data=${DATASET}
