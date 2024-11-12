b51dd9044f1fcf002ea9e1d38728f26a81cf6f73
zhangzw16 <thuzhangzw@outlook.com>
2024-11-12 02:23:15 +0000

    update finetuning configurations and add new weighted datasets

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,12 +1,12 @@
 DATA=${1}
 # VAL_DATA=${2}
 # MODEL=${3:-moirai_1.0_R_small}
-MODEL=moirai_1.0_R_small
+MODEL=moirai_1.1_R_base
 
 python -m cli.train \
   -cp conf/finetune \
   --config-name cpt.yaml \
-  run_name=${MODEL}_ft_${DATA}_lr_1em4_seed_321 \
+  run_name=${MODEL}_ft_${DATA}_lr_1em4 \
   model=${MODEL} \
   data=${DATA} \
   # val_data=${VAL_DATA}
